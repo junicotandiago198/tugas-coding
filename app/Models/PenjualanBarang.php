060749626e09e88c30f99aaecf8cc5770cfddbae
junicotandiago198 <junicotandiagoo21@gmail.com>
2023-09-19 22:35:34 +0700

    add: fillable model

--- app/Models/PenjualanBarang.php
+++ app/Models/PenjualanBarang.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class PenjualanBarang extends Model
 {
     use HasFactory;
+
+    protected $table = 'penjualan_barang';
+
+    protected $fillable = [
+        'nama_barang',
+        'stok',
+        'jumlah_terjual',
+        'tanggal_transaksi',
+        'jenis_barang',
+    ];
 }
